4e34f12d851b213230ec5f2be77ae4b879bb5024
abdulkhalimov <adamabdulkhalimov@mail.ru>
2021-04-11 16:15:41 +0300

    home work 2

---
+++ js/homework2.js
@@ -0,0 +1,69 @@
+// типы данных
+// number: 10 , -142, 20.45, -12.21, NaN ,
+// infinity ;
+// string: " 14424", "alvi , i am ready"
+// boolean(логический): true/false
+
+let a = 12; // number
+let b = 'lorem'; //string
+let c = false; // boolean
+
+console.log (typeof false)
+
+//математические операторы 
+// + - * / %
+
+//let x = 10;
+//let y = 'ipsum';
+//let result = x + y;
+//console.log (result)
+
+// true = 1;
+// false = 0;
+
+// Oператоры сравнения ;
+// >,<, ==, >=, <=, !=; === - оператор стрг;
+
+//let x = 100 ; // str
+//let y = 2 ; // str
+
+//let result = x > y;
+
+//console.log(result)
+
+//чем позже буква по алфавиту , тем оно тяжелее;
+
+//let userPass = prompt('password');
+//let pass = 'admin';
+//let result = userPass == pass;
+//console.log (result)
+
+//let name = 'ADAM';
+//let age = 19;
+//let country = 'Chechnya))'
+//let presentation = `Hello I'am ${name}. I'm ${age} years old. I'm from ${country}`
+//console.log (presentation) 
+
+
+
+//HOME WORK #2 
+// -1) для испольования даты нужен СТРОЧНЫЙ тип данных
+//let date = "28,02,1999";
+//console.log (date)
+// -2) для сохранения числа пи используют номерной тип данных флоат;
+//const f = 17;
+//let x = 13;
+//let result = f * x;
+//console.log (result)
+
+//let name = prompt();
+//let adress = prompt();
+//let phone = prompt();
+
+//let presentation = `student ${name}. adress ${adress}. phone number ${phone}`;
+
+//console.log(presentation)
+let dol = 78;
+let rub = 1000;
+
+console.log (rub/dol);
